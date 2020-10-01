f80de796f905f8eefdd64b76c40b56adf8121ed5
ckomodo <chrissakwa@gmail.com>
2020-10-01 00:01:50 -0700

    js file, readme set up, packages installed, instructions for homework written in comments

---
+++ index.js
@@ -0,0 +1,18 @@
+//TODO: npm init
+//TODO: install dependencies - inquirer
+//TODO: require all dependencies I'll need - fs
+//TODO: create an array of questions
+//TODO: write a readme in a markdown file as a template (copy/paste into readme gen function)
+//TODO: function that will generate my readme template
+//TODO: use inquirer to prompt users with questions(questions from readme file in class repo)
+//TODO: object of answers expected from inquirer, use these answers to pass into my readme gen function. Check module.exports
+//TODO:write file using template generated from readme function
+//TODO: use back ticks e.g. return `# ${data.title}`
+//TODO:
+//TODO:
+//TODO:
+//TODO:
+//TODO:
+//TODO:
+//TODO:
+//TODO:
